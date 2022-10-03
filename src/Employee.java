825c6a587cadd6c865010aa0ae0cb7b160cc0b8c
Lex <matveychuk.m@gmail.com>
2022-10-03 21:16:29 +0300

    Created class main and encapsulated Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,20 +1,72 @@
 public class Employee {
 
 
-        private String fullName;
-        private String position;
-        private String email;
-        private String phone;
-        private int age;
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int age;
 
-        public Employee(String fullName, String position, String email, String phone, int age) {
-                this.fullName = fullName;
-                this.position = position;
-                this.email = email;
-                this.phone = phone;
-                this.age = age;
-        }
 
+    private Employee(String fullName, String position, String email, String phone, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+    static Employee employee2 = new Employee("Sidorov Oleg Olegovich", "Dantist",
+            "dantist@gmail.com", "0932223344", 20);
+    private Employee() {
+        this.fullName = "Ivanov Ivan Ivanovich";
+        this.position = "Tailor";
+        this.email = "tailor@gmail.com";
+        this.phone = "0677689876";
+        this.age = 37;
+
+    }
+
+    static Employee employee = new Employee();
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 
 }
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Full name: " + Employee.employee2.getFullName() +
+                "\nPosition: " + Employee.employee2.getPosition() +
+                "\nEmail: " + Employee.employee2.getEmail() +
+                "\nPhone: " + Employee.employee2.getPhone() +
+                "\nAge: " + Employee.employee2.getAge() +
+                "\n_______________________");
+
+
+        System.out.println("Full name: " + Employee.employee.getFullName() +
+                "\nPosition: " + Employee.employee.getPosition() +
+                "\nEmail: " + Employee.employee.getEmail() +
+                "\nPhone: " + Employee.employee.getPhone() +
+                "\nAge: " + Employee.employee.getAge() +
+                "\n_______________________");
+    }
+}
