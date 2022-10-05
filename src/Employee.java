c9bd97de4be493ddec9b7a60013b529369016644
NewEnergyCode <matveychuk.m@gmail.com>
2022-10-05 20:18:00 +0300

    Make non static Employee in main

--- src/Employee.java
+++ src/Employee.java
@@ -8,25 +8,14 @@ public class Employee {
     private int age;
 
 
-    private Employee(String fullName, String position, String email, String phone, int age) {
+    public Employee(String fullName, String position, String email, String phone, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
         this.phone = phone;
         this.age = age;
     }
-    static Employee employee2 = new Employee("Sidorov Oleg Olegovich", "Dantist",
-            "dantist@gmail.com", "0932223344", 20);
-    private Employee() {
-        this.fullName = "Ivanov Ivan Ivanovich";
-        this.position = "Tailor";
-        this.email = "tailor@gmail.com";
-        this.phone = "0677689876";
-        this.age = 37;
 
-    }
-
-    static Employee employee = new Employee();
 
     public String getFullName() {
         return fullName;
