d5f77335fd739e604c778fa6a5ed8f43f66586a4
MI <matveychuk.m@gmail.com>
2022-11-02 23:14:12 +0300

    add CountingWordsInBook add count words

--- src/Main.java
+++ src/Main.java
@@ -18,25 +18,25 @@ public class Main {
         bookLibrary.put("ГИД JAVA", new File("src/Library/ГИД JAVA.txt"));
 
 
-        try (FileReader fileReader = new FileReader(bookLibrary.get("ГИД JAVA"));
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            Map<String, Integer> uniqueWords = new HashMap<>();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] words = cwb.removeExtraCharacters(line);
-
-                for (String s : words) {
-                    if (s.length() < 3) continue;
-                    if (uniqueWords.containsKey(s)) uniqueWords.put(s, uniqueWords.get(s) + 1);
-                    else uniqueWords.put(s, 1);
-                }
-
-            }
-            System.out.println(uniqueWords);
-        } catch (IOException e) {
-            System.out.println("File not found!");
-            throw new RuntimeException(e);
-        }
+//        try (FileReader fileReader = new FileReader(bookLibrary.get("ГИД JAVA"));
+//             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+//            Map<String, Integer> uniqueWords = new HashMap<>();
+//            String line;
+//            while ((line = bufferedReader.readLine()) != null) {
+//                String[] words = cwb.removeExtraCharacters(line);
+//
+//                for (String s : words) {
+//                    if (s.length() < 3) continue;
+//                    if (uniqueWords.containsKey(s)) uniqueWords.put(s, uniqueWords.get(s) + 1);
+//                    else uniqueWords.put(s, 1);
+//                }
+//
+//            }
+//            System.out.println(uniqueWords);
+//        } catch (IOException e) {
+//            System.out.println("File not found!");
+//            throw new RuntimeException(e);
+//        }
 
     }
 }
