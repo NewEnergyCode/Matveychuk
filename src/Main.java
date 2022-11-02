e1273bf0536de057c58cb3992294d1619e741559
MI <matveychuk.m@gmail.com>
2022-11-03 00:04:48 +0300

    add CountingWordsInBook add count words add scanner

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,20 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        CountingWordsInBook cwb = new CountingWordsInBook();
-
         Map<String, File> bookLibrary = new HashMap<>();
         bookLibrary.put("ГИД JAVA", new File("src/Library/ГИД JAVA.txt"));
 
-        CountingWordsInBook countingWordsInBook = new CountingWordsInBook();
-        System.out.println(countingWordsInBook.uniqueWordsCounter(bookLibrary.get("ГИД JAVA")));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter book name: ");
+        String bookName = scanner.nextLine();
 
+        if (bookLibrary.containsKey(bookName)) {
+            CountingWordsInBook cwb = new CountingWordsInBook();
+            CountingWordsInBook countingWordsInBook = new CountingWordsInBook();
+            System.out.println(countingWordsInBook.uniqueWordsCounter(bookLibrary.get("ГИД JAVA")));
+        }else {
+            System.out.println("Your book is not found!");
+        }
     }
 }
 
