e5a725a45d1326e2f1c3d2506fe47f8af07a667e
MI <matveychuk.m@gmail.com>
2022-11-02 23:14:12 +0300

    add CountingWordsInBook add count words

--- src/CountingWordsInBook.java
+++ src/CountingWordsInBook.java
@@ -1,10 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class CountingWordsInBook {
     private static final String bookDividersForSplit = "•!,[].{}:;«»/`'’()<>?*|\"~•";
 
 
+    public Map<String, Integer> uniqueWordsCounter(File file) {
+        Map<String, Integer> uniqueWords = new HashMap<>();
+        try (FileReader fileReader = new FileReader(file) ;
+        BufferedReader bufferedReader = new BufferedReader(fileReader)){
 
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = removeExtraCharacters(line);
 
+                for (String s : words) {
+                    if (s.length() < 3) continue;
+                    if (uniqueWords.containsKey(s)) uniqueWords.put(s, uniqueWords.get(s) + 1);
+                    else uniqueWords.put(s, 1);
+                }
 
+            }
+
+        } catch(IOException e){
+            System.out.println("File not found!");
+            throw new RuntimeException(e);
+        }
+        return uniqueWords;
+    }
 
 
     public String[] removeExtraCharacters(String line) {
@@ -13,4 +39,24 @@ public class CountingWordsInBook {
         }
         return line.toLowerCase().split(" ");
     }
+
+
+    public List<BooksLibrary> sortBySize(File file) {
+
+        List<BooksLibrary> sortBySize = new ArrayList<>();
+        for (String key : .keySet()) {
+            sortBySize.addAll(fileListMap.get(key));
+        }
+        Collections.sort(sortBySize, new Comparator<FileData>() {
+            @Override
+            public int compare(FileData o1, FileData o2) {
+                return o1.getSizeInByte() - o2.getSizeInByte();
+            }
+        });
+
+
+        return sortBySize;
+    }
+
+
 }
