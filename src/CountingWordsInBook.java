94ff41743ff68bf3e4667a3479cccc6c67f2d753
MI <matveychuk.m@gmail.com>
2022-11-06 16:04:34 +0200

    add statistic writer

--- src/CountingWordsInBook.java
+++ src/CountingWordsInBook.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class CountingWordsInBook {
     private static final String bookDividersForSplit = "•!,[].{}:;«»/`'’()<>?*|\"~•";
-
     private final Map<String, Integer> uniqueWords = new HashMap<>();
-    FileOutputStream fileOutputStream;
-
+    private String path;
 
     public void uniqueWordsCounter(File file) {
-
+        if (file.getName().equals("Verse.txt")) path = "src/Library/Verse_statistic.txt";
+        else if (file.getName().equals("New Book.txt")) path = "src/Library/New Book_statistic.txt";
+        else if (file.getName().equals("ГИД JAVA.txt")) path = "src/Library/ГИД JAVA_statistic.txt";
+        System.out.println(path);
         try (FileReader fileReader = new FileReader(file);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
 
@@ -22,14 +23,12 @@ public class CountingWordsInBook {
                     if (uniqueWords.containsKey(s)) uniqueWords.put(s, uniqueWords.get(s) + 1);
                     else uniqueWords.put(s, 1);
                 }
-
             }
-
         } catch (IOException e) {
             System.out.println("File not found!");
             throw new RuntimeException(e);
         }
-
+        sortBooksAndPrintMorePopulare();
     }
 
 
@@ -40,7 +39,7 @@ public class CountingWordsInBook {
         return line.toLowerCase().split(" ");
     }
 
-    public void sortBooksAndPrintMorePorulare() {
+    public void sortBooksAndPrintMorePopulare() {
 
         List<Map.Entry<String, Integer>> sortAmount = new ArrayList<>(uniqueWords.entrySet());
         sortAmount.sort(new Comparator<Map.Entry<String, Integer>>() {
@@ -50,28 +49,39 @@ public class CountingWordsInBook {
             }
         }.reversed());
         printFirstTenWords(sortAmount);
-
     }
-
 
     public void printFirstTenWords(List<Map.Entry<String, Integer>> list) {
-        int a = 0;
-        int b = 0;
-        for (Map.Entry<String, Integer> entry : list) {
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            System.out.println(a + 1 + ". " + key + ": " + value);
-            a++;
 
-            if (a > 9)
-                break;
-        }
-        for (Map.Entry<String, Integer> i : list) {
-            if (i.getValue().equals(1)) b++;
-        }
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(path);
 
-        System.out.println("Uniqueness words: " + b);
+            int a = 0;
+            int b = 0;
+
+            for (Map.Entry<String, Integer> entry : list) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                System.out.println(a + 1 + ". " + key + ": " + value);
+                String aa = "\n" + (a + 1) + ". " + key + ": " + value;
+                try {
+                    fileOutputStream.write(aa.getBytes());
+                } catch (IOException e) {
+                    System.out.println("File not found!");
+                    throw new RuntimeException(e);
+                }
+                a++;
+                if (a > 9)
+                    break;
+            }
+            for (Map.Entry<String, Integer> i : list) {
+                if (i.getValue().equals(1)) b++;
+            }
+            System.out.println("Uniqueness words: " + b);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            throw new RuntimeException(e);
+        }
     }
-
 
 }
